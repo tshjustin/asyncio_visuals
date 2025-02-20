7ec1fddceca163062c6dec8aa272f13bfdde3995
tshjustin <j.tann2305@gmail.com>
2025-02-20 19:09:12 +0800

    Minor bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'asyncio_visuals',
+  base: '/asyncio_visuals',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
